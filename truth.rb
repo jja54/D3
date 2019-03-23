9cefb06ebac21133a1f22a18592d2d275115a0de
Jack Anderson <jja54@pitt.edu>
2019-03-23 16:29:19 -0400

    init

--- truth.rb
+++ truth.rb
@@ -6,7 +6,7 @@ def display_param_error
 end
 
 def parameters_valid(true_sym, false_sym, size)
-  if true_sym == false_sym && !true_sym.nil? && !false_sym.nil? || size.to_i < 2 && !size.nil?
+  if true_sym == false_sym && !true_sym.nil? && !false_sym.nil? || size.to_i < 2 && !size.nil? || true_sym.length > 1 || false_sym.length > 1
 	false
   else
     true
@@ -23,13 +23,12 @@ end
 
 def generate_table(true_sym, false_sym, size)
   table_lines = 2**size.to_i # 2^n values, n bit string
-  table = Array.new(table_lines) { Array.new(size.to_i) } # 2^n by n table
-  puts 
+  table = Array.new(table_lines) { Array.new(size.to_i-1) } # 2^n by n table
   # Populate base table lines
-  for i in 0..table_lines
-    binary_str = i.to_s(2)
-    for j in 0..size.to_i
-      if binary_str.slice(j) == 0
+  for i in 0..table_lines-1
+    binary_str = i.to_s(2).rjust(size.to_i, '0')
+    for j in 0..size.to_i-1
+      if binary_str.slice(j) == '0'
         table[i][j] = false
       else
         table[i][j] = true
@@ -38,34 +37,34 @@ def generate_table(true_sym, false_sym, size)
   end
   # Now parse each line per its logical operators
   output = Array.new(table_lines) { Array.new(4) } # 4 logical operators 
-  for i in 0..table_lines
-    for j in 0..size.to_i
+  for i in 0..table_lines-1
+    for j in 0..4
       if j == 0 # AND
         if i == table_lines-1
-          output[i][j] = true
+          output[i][0] = true
         else
-          output[i][j] = false
+          output[i][0] = false
         end
 
       elsif j == 1 # OR
         if i > 0
-          output[i][j] == true
+          output[i][1] = true
         else
-          output[i][j] = false
+          output[i][1] = false
         end
-
       elsif j == 2 # NAND
         if output[i][0] == true
-          output[i][j] = false
+          output[i][2] = false
         else
-          output[i][j] = true
+          output[i][2] = true
         end
 
       elsif j == 3 # NOR
+	    puts output[i][1]
         if output[i][1] == true
-          output[i][j] = false
+          output[i][3] = false
         else
-          output[i][j] = true
+          output[i][3] = true
         end
       end
     end
@@ -87,19 +86,27 @@ get '/' do
 	erb :index
 end
 
-get '/display' do
+post '/display' do
   # Get the parameters
   true_sym = params['ts']
   false_sym = params['fs']
   size = params['s']
-  unless parameters_entered(true_sym, false_sym, size)
-    # Default parameters
+  # Default parameters
+  if true_sym.nil? || true_sym == ''
+    puts 'true is nil'
     true_sym = 'T'
+  end
+  if false_sym.nil? || false_sym == ''
     false_sym = 'F'
+  end
+  if size.nil? || size == ''
     size = 3
   end
-  puts true_sym, false_sym, size
-  returned = generate_table(true_sym, false_sym, size)
-  erb :display, :locals => { true_sym: true_sym, false_sym: false_sym, size: size.to_i, 
+  if parameters_valid(true_sym, false_sym, size)
+    returned = generate_table(true_sym, false_sym, size)
+    erb :display, :locals => { true_sym: true_sym, false_sym: false_sym, size: size.to_i, 
       table: returned[0], output: returned[1] }
+  else
+    erb :error
+  end
 end
